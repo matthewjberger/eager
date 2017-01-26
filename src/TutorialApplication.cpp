e49ce68265904fcecee8591aaf47be2e6551af01
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-01-26 15:12:04 -0800

    Update source to use tutorial 1's code.

--- src/TutorialApplication.cpp
+++ src/TutorialApplication.cpp
@@ -1,37 +1,49 @@
-/*
------------------------------------------------------------------------------
-Filename:    TutorialApplication.cpp
------------------------------------------------------------------------------
-
-This source file is part of the
-   ___                 __    __ _ _    _
-  /___\__ _ _ __ ___  / / /\ \ (_) | _(_)
- //  // _` | '__/ _ \ \ \/  \/ / | |/ / |
-/ \_// (_| | | |  __/  \  /\  /| |   <| |
-\___/ \__, |_|  \___|   \/  \/ |_|_|\_\_|
-      |___/
-Tutorial Framework (for Ogre 1.9)
-http://www.ogre3d.org/wiki/
------------------------------------------------------------------------------
-*/
-
 #include "TutorialApplication.h"
 
-//---------------------------------------------------------------------------
 TutorialApplication::TutorialApplication(void)
 {
 }
-//---------------------------------------------------------------------------
+
 TutorialApplication::~TutorialApplication(void)
 {
 }
 
-//---------------------------------------------------------------------------
 void TutorialApplication::createScene(void)
 {
-    // Create your scene here :)
+  mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
+
+  mCamera->setPosition(0, 47, 222);
+
+  Ogre::Light* light = mSceneMgr->createLight("MainLight");
+  light->setPosition(20.0, 80.0, 50.0);
+
+  Ogre::Entity* ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
+
+  Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+  ogreNode->attachObject(ogreEntity);
+
+  Ogre::Entity* ogreEntity2 = mSceneMgr->createEntity("ogrehead.mesh");
+
+  Ogre::SceneNode* ogreNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode(
+    Ogre::Vector3(84, 48, 0));
+  ogreNode2->attachObject(ogreEntity2);
+
+  Ogre::Entity* ogreEntity3 = mSceneMgr->createEntity("ogrehead.mesh");
+
+  Ogre::SceneNode* ogreNode3 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+  ogreNode3->setPosition(Ogre::Vector3(0, 104, 0));
+  ogreNode3->setScale(2, 1.2, 1);
+  ogreNode3->attachObject(ogreEntity3);
+
+  Ogre::Entity* ogreEntity4 = mSceneMgr->createEntity("ogrehead.mesh");
+
+  Ogre::SceneNode* ogreNode4 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+  ogreNode4->setPosition(-84, 48, 0);
+  ogreNode4->roll(Ogre::Degree(-90));
+  ogreNode4->attachObject(ogreEntity4);
+
 }
-//---------------------------------------------------------------------------
+
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -48,25 +60,23 @@ extern "C" {
     int main(int argc, char *argv[])
 #endif
     {
-        // Create application object
-        TutorialApplication app;
+  // Create application object
+  TutorialApplication app;
 
-        try {
-            app.go();
-        } catch(Ogre::Exception& e)  {
+  try {
+      app.go();
+  } catch(Ogre::Exception& e)  {
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-            MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+      MessageBox(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
 #else
-            std::cerr << "An exception has occurred: " <<
-                e.getFullDescription().c_str() << std::endl;
+      std::cerr << "An exception has occurred: " <<
+    e.getFullDescription().c_str() << std::endl;
 #endif
-        }
+  }
 
-        return 0;
+  return 0;
     }
 
 #ifdef __cplusplus
 }
 #endif
-
-//---------------------------------------------------------------------------
