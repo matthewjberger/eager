062d5a4eb2211e36c713e3c0baf32d43c65bf1f8
Matthew J. Berger <matthewberger@nevada.unr.edu>
2017-01-28 22:10:45 -0800

    Add dir-locals file for emacs.

---
+++ .dir-locals.el
@@ -0,0 +1,20 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (helm-make-build-dir . "build/")
+  (flycheck-clang-language-standard . "c++14")
+  (flycheck-clang-include-path
+  "/usr/include/ois"
+  "/usr/include/boost"
+  "/usr/local/include/OGRE"
+  "/usr/local/include/OGRE/Paging"
+  "/usr/local/include/OGRE/Plugins/PCZSceneManager"
+  "/usr/local/include/OGRE/Plugins/OctreeSceneManager"
+  "/usr/local/include/OGRE/Plugins/ParticleFX"
+  "/usr/local/include/OGRE/Plugins/OctreeZone"
+  "/usr/local/include/OGRE/Plugins/BSPSceneManager"
+  "/usr/local/include/OGRE/RTShaderSystem"
+  "/usr/local/include/OGRE/RenderSystems/GL"
+  "/usr/local/include/OGRE/Terrain"
+)))
